b3221abf69ce4c6821c2128d48d98e022a7e5273
Appslk <162972386+appslk@users.noreply.github.com>
2025-06-28 15:00:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 
- 					<Route path='/' element={<CarrotechChecker />} />
+ 					<Route path='/' element={<Home />} />
 
 				</Routes>
 			</BrowserRouter>
